0eab4d24676fa7e63dfc227d09122e80d8d58ac7
Jayson <96589775+Jayson-Amike@users.noreply.github.com>
2025-11-25 16:29:58 -0500

    made a navbar for the admin and updaed the css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,21 +2,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./components/Auth/AuthPage";
 import Dashboard from "./components/Dashboard/Dashboard";
 import ProtectedRoute from "./components/Dashboard/ProtectedRoute";
+import AdminPage from "./components/Admin/AdminPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<AuthPage />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
+    <AdminPage />
   );
 }
---
+++ my-react-app/src/components/Admin/AdminPage.jsx
@@ -0,0 +1,10 @@
+import AdminPanel from "./AdminPanel"
+import AdminNavBar from "../Navbar/AdminNavBar"
+export default function AdminPage() {
+    return(
+        <div>
+            <AdminNavBar />
+            <AdminPanel />
+        </div>
+    )
+}
